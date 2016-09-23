bc19f44b09582d82e7a11d193f94b9f4cba87b93
Harshavardhan Patil <patil.bmsce@gmail.com>
2016-09-23 16:42:35 -0500

    Added implementation file storage_mgr.c

---
+++ storage_mgr.c
@@ -0,0 +1,10 @@
+//
+// Created by Harshavardhan Patil on 9/23/16.
+//
+// This file has implementation of all the interfaces mentioned in storage_mgr.h
+#include <stdio.h>
+#include <storage_mgr.h>
+#include "storage_mgr.h"
+
+void initStorageManager(){
+}
