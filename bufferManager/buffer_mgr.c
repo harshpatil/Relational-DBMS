273f5787caa0f1725500257cf562a05f3d002e6f
Harshavardhan Patil <patil.bmsce@gmail.com>
2016-10-26 14:38:11 -0500

    Added frameNode tsructure

--- bufferManager/buffer_mgr.c
+++ bufferManager/buffer_mgr.c
@@ -1,4 +1,29 @@
 //
 // Created by Harshavardhan Patil on 10/26/16.
 //
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "buffer_mgr.h"
+#include "dberror.h"
+#include "storage_mgr.h"
 
+typedef struct frameNode{
+
+    int pageNumber;
+    int frameNumber;
+    int dirty;
+    int fixCount;
+    int pageFrequency;
+    char *data;
+    struct frameNode *next;
+    struct frameNode *previous;
+
+}frameNode;
+
+typedef struct frameList{
+
+    frameNode *first;
+    frameNode *last;
+
+}frameList;
