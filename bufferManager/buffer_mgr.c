798f96f0d63f19bcb3c527146b190c5ee7692c66
Sowmya Parameshwara <sowmyaparameshwara@Sowmyas-MacBook-Air.local>
2016-10-26 14:39:44 -0500

    Adding Buffer Manager Info Structure

--- bufferManager/buffer_mgr.c
+++ bufferManager/buffer_mgr.c
@@ -1,4 +1,41 @@
 //
 // Created by Harshavardhan Patil on 10/26/16.
 //
+#include <stdlib.h>
+#include "buffer_mgr.h"
+#include "storage_mgr.h"
 
+#define MAX_FRAMES 100
+
+// convenience macros
+#define MAKE_BUFFER_MANAGER_INFO()					\
+  ((BufferManagerInfo *) malloc (sizeof(BufferManagerInfo)))
+
+
+/* Additional data which will be required per buffer pool. Will be attached to BM_BufferPool->mgmtData*/
+typedef struct BufferManagerInfo{
+    int numOfDiskReads;
+    int numOfDiskWrites;
+    int frameToPageId[MAX_FRAMES];
+    bool dirtyFrames[MAX_FRAMES];
+    int pinCountsPerFrame[MAX_FRAMES];
+    FrameNode *headFrameNode;
+    FrameNode *tailFrameNode;
+}BufferManagerInfo;
+
+// Buffer Manager Interface Pool Handling
+RC initBufferPool(BM_BufferPool *const bm, const char *const pageFileName,
+                  const int numPages, ReplacementStrategy strategy,
+                  void *stratData){
+    SM_FileHandle fileHandle;
+    int status;
+    if(bm->pageFile==NULL || numPages <= 0){
+        return RC_BM_INVALID_PAGE;
+    }
+    if ((status = openPageFile ((char *)pageFileName, &fileHandle)) != RC_OK){
+        return status;
+    }
+    BufferManagerInfo *bmInfo = MAKE_BUFFER_MANAGER_INFO();
+
+
+}
